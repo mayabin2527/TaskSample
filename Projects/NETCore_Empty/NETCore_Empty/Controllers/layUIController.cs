feeee22fe4c63bee74a92850c8d414467292b9c0
“mayabin2527” <846209981@qq.com>
2022-04-19 23:21:06 +0800

    UPDATE

--- Projects/NETCore_Empty/NETCore_Empty/Controllers/layUIController.cs
+++ Projects/NETCore_Empty/NETCore_Empty/Controllers/layUIController.cs
@@ -13,7 +13,15 @@ namespace NETCore_Empty.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Slist> l = new List<Slist>();
+            Slist a = new Slist() {title= "item 1" , id= "aaa" };
+
+            ViewBag.List = @"[{title: 'item 1' ,id: 'aaa' }, {title: 'item 2',id: 'bbb' }]";
             return View();
+        }
+        public class Slist {
+            public string title { set; get; }
+            public string id { set; get; }
         }
     }
 }
